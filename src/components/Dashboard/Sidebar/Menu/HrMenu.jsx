0221d2a73c80804d1679c67763104d9d8405181a
younusFoysal <younus.faysal@gmail.com>
2024-06-04 10:52:23 +0600

    modal fix

--- src/components/Dashboard/Sidebar/Menu/HrMenu.jsx
+++ src/components/Dashboard/Sidebar/Menu/HrMenu.jsx
@@ -13,34 +13,9 @@ const HrMenu = () => {
     const axiosSecure = useAxiosSecure()
     const { user } = useAuth()
     const [role] = useRole()
-    // for modal
-    const [isModalOpen, setIsModalOpen] = useState(false)
-    const closeModal = () => {
-        setIsModalOpen(false)
-    }
+    
 
-    const modalHandler = async () => {
-        console.log('I want to be a host')
-        try {
-            const currentUser = {
-                email: user?.email,
-                role: 'guest',
-                status: 'Requested',
-            }
-            const { data } = await axiosSecure.put(`/user`, currentUser)
-            console.log(data)
-            if (data.modifiedCount > 0) {
-                toast.success('Success! Please wait for admin confirmation')
-            } else {
-                toast.success('Please!, Wait for admin approval👊')
-            }
-        } catch (err) {
-            console.log(err)
-            toast.error(err.message)
-        } finally {
-            closeModal()
-        }
-    }
+    
     return (
         <>
 
